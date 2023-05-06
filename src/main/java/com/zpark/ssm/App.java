6fb833858f81b1ac29a158739f9cc7f7f48980ed
jyw <jyw@163.com>
2023-05-06 09:05:04 +0800

    first commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args){
         System.out.println("Hello Git!");
         System.out.println("Hello Git! jyw master second commit!");
+        System.out.println("Hello Git! jyw hotfix first commit!");
     }
 }
